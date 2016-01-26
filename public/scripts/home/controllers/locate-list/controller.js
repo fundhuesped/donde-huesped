e575cb36e817bdae7c81b1f51285228db12f5149
Tincho <tincho>
2016-01-25 22:31:35 -0300

    mejoras de workflow y base de datos inicial

--- public/scripts/home/controllers/locate-list/controller.js
+++ public/scripts/home/controllers/locate-list/controller.js
@@ -6,13 +6,16 @@ dondev2App.controller('locateListController',
 	$scope.places = [];
 	$scope.main = true;
 	var onLocationError = function(e){
-    	$timeout.cancel($rootScope.promiseHelp);
-    	$location.path('/call/help');
+		  	$scope.$apply(function(){
+    			$location.path('/call/help');
+    		});
   	}
   	var onLocationFound = function(position){
-    	placesFactory.forLocation(position.coords, function(result){ 
-          $scope.places = $scope.closer = result;
-          $rootScope.currentPos = position.coords;
+  		$scope.$apply(function(){
+	    	placesFactory.forLocation(position.coords, function(result){ 
+	          $scope.places = $scope.closer = result;
+	          $rootScope.currentPos = position.coords;
+	        });
         });
     };
     navigator.geolocation.getCurrentPosition(onLocationFound, onLocationError);
--- public/scripts/home/controllers/locate-map/controller.js
+++ public/scripts/home/controllers/locate-map/controller.js
@@ -6,16 +6,20 @@ dondev2App.controller('locateMapController',
 	$scope.places = [];
 	$scope.main = true;
   
-	var onLocationError = function(e){
-    	$timeout.cancel($rootScope.promiseHelp);
-    	$location.path('/call/help');
-  	}
-  	var onLocationFound = function(position){
-    	placesFactory.forLocation(position.coords, function(result){ 
-          $scope.places = $scope.closer = result;
-          
-          var map = NgMap.initMap('mainMap');
-          $scope.currentPos = position.coords;
+	 var onLocationError = function(e){
+        $scope.$apply(function(){
+          $location.path('/call/help');
+        });
+    };
+
+    var onLocationFound = function(position){
+      $scope.$apply(function(){
+        	placesFactory.forLocation(position.coords, function(result){ 
+              $scope.places = $scope.closer = result;
+              
+              var map = NgMap.initMap('mainMap');
+              $scope.currentPos = position.coords;
+            });
         });
     };
 
