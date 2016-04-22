a406ec896b32e15d55f1e153428ca967f1e6b8b9
tinchoforever <tinchoforever@gmail.com>
2016-04-22 16:51:17 -0300

    me habia olvidado el share

--- resources/views/share.blade.php
+++ resources/views/share.blade.php
@@ -8,7 +8,7 @@
     <meta property='og:locale' content='es_LA'/>
     <meta property='og:title' content='donde.huesped.org.ar | Fundación Huésped'/>
     <meta property="og:description" ccontent="{{$p->establecimiento}} en {{$p->nombre_partido}}, {{$p->calle}} {{$p->altura}}  ">
-    <meta property='og:url' content='https://donde.herokuapp.com/share/{{$p->idPlace}} '/>
+    <meta property='og:url' content='https://donde.herokuapp.com/share/{{$p->placeId}} '/>
     <meta property='og:site_name' content='Fundación Huésped'/>
     <meta property='og:type' content='website'/>
     <meta property='og:image' content='{!! $p->image !!}'/>
@@ -16,7 +16,7 @@
     <meta name="twitter:card" content="summary">
     <meta name='twitter:title' content='donde.huesped.org.ar | Fundación Huésped'/>
     <meta name="twitter:description" content="{{$p->establecimiento}} en {{$p->nombre_partido}}, {{$p->calle}} {{$p->altura}}  ">
-    <meta name='twitter:url' content='https://donde.herokuapp.com/share/{{$p->idPlace}} '/>
+    <meta name='twitter:url' content='https://donde.herokuapp.com/share/{{$p->placeId}} '/>
     <meta name='twitter:image' content='{!! $p->image !!}'/>
     <meta name='twitter:site' content='@fundhuesped' />
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
