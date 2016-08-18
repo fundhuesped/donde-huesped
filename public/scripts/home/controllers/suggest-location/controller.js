e07405abf8b2d339f3f662d47a40e201d8ebbdf3
Franco Rabaglia <francoo.hd@gmail.com>
2016-08-18 14:48:24 -0300

    fixed

--- public/scripts/home/controllers/suggest-location/controller.js
+++ public/scripts/home/controllers/suggest-location/controller.js
@@ -1,6 +1,5 @@
 dondev2App.controller('locationNewController',
     function($timeout, copyService, placesFactory, NgMap, $scope, $rootScope, $routeParams, $location, $http) {
-        // $rootScope.navBar = $routeParams.servicio;
-        // $scope.service = copyService.getFor($routeParams.servicio);
-        $scope.pablo = "ricardo";
+        $rootScope.navBar = $routeParams.servicio;
+        $scope.service = copyService.getFor($routeParams.servicio);
 });
