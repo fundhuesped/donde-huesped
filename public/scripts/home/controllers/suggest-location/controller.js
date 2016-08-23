af38e20b3b7595254851007eb0d0647e87b9e86f
jona_872 <jona_872@hotmail.com>
2016-08-23 11:11:53 -0300

    preUpload

--- public/scripts/home/controllers/suggest-location/controller.js
+++
@@ -1,5 +0,0 @@
-dondev2App.controller('locationNewController',
-    function($timeout, copyService, placesFactory, NgMap, $scope, $rootScope, $routeParams, $location, $http) {
-        $rootScope.navBar = $routeParams.servicio;
-        $scope.service = copyService.getFor($routeParams.servicio);
-});
