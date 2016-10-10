a331a72f43bc5ba458960c65e69fa963333a90f9
jona_872 <jona_872@hotmail.com>
2016-10-10 14:12:19 -0300

    sinc

--- donde-laravel-server/resources/views/panel/importer/picker.blade.php
+++ donde-laravel-server/resources/views/panel/importer/picker.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.panel-master')
-{{-- {!!Html::style('styles/import.min.css')!!} --}}
+{!!Html::style('styles/import.min.css')!!}
 {!!Html::style('bower_components/materialize/bin/materialize.css')!!}
 {!!Html::script('bower_components/materialize/bin/materialize.js')!!}
 
@@ -8,29 +8,23 @@
 <br>
 
 
-    <div class="row">
-        <div class="col s12">
-            <h5 class="titulo">
-                Seleccione archivo a importar (.csv)
-            </h5>
+<div class="row">
+            <div class="col s12">
+                <h5 class="titulo">
+                    Seleccione archivo a importar (.csv)
+                </h5>
+            </div>      
         </div>      
-    </div>      
 
             {!!Form::open(['url'=>'panel/importer/preview', 'method'=>'POST','files'=>true])!!}
-                <div class="container">
-                    <div class="row">
-                        <div class="col s6 offset-s3">
-                            {!!Form::label('file','FILE:')!!}
-                        </div>
-                    </div>
-                        
-                    <div class="row">    
+                <div class="container centrada">
+                    <div class="row centrada">    
                         <div class="col s12">
                             {!!Form::file('file')!!}
                         </div>
                     </div>
-                    <div class="row">  
-                        <div class="col s6 offset-s3">
+                    <div class="row centrada">  
+                        <div class="col s12">
                             {!!Form::submit('Siguiente',['class'=>'btn','id'=>'submit'])!!}
                         </div>
                     </div>
@@ -38,27 +32,6 @@
 
             {!!Form::close()!!}
             {{-- <button id="jona">adsad</button> --}}
-<form action="#panel/importer/preview" method="POST" >
-    <div class="file-field input-field">
-        <div class="row col s9 offset-s3">
-
-
-
-          <div class="btn col s3">
-            <span>Seleccionar archivo</span>
-            <input type="file">
-        </div>
-        
-        <div class="file-path-wrapper col s6">
-            <input class="file-path validate" type="text" placeholder=" Ningun archivo seleccionado">
-        </div>
-
-
-    </div>
-</div>
-</form>
-
-
 
 
 @endsection
