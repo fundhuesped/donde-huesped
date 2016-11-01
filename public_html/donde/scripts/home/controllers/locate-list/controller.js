d28dacac8f0d7f3a82920569a14f6cfa7f025604
jona_872 <jona_872@hotmail.com>
2016-11-01 01:36:02 -0300

    sinc

--- public_html/donde/scripts/home/controllers/locate-list/controller.js
+++ public_html/donde/scripts/home/controllers/locate-list/controller.js
@@ -1,4 +1,4 @@
-dondev2App.controller('locateListController', 
+dondev2App.controller('locateListController',
 	function(placesFactory,NgMap, $scope,$rootScope, $routeParams, $location, $http){
 	$rootScope.main = false;
 	$scope.service = $routeParams.servicio;
@@ -7,40 +7,45 @@ dondev2App.controller('locateListController',
 	$scope.main = true;
 	$rootScope.geo = true;
 	$scope.loading = true;
-
+	// $scope.cantidad = 0;
 	//parseo a obj para obtener el servicio
 	$scope.service = angular.fromJson($scope.service);
-	
+
 	//seteo a todos en false x las dudas
 	$scope.checkbox = false;
-	
+
 	$scope.$watchCollection('checkbox', function(newValue, oldValue) {
 		$scope.checkbox = newValue;
-		if ($scope.checkbox) { 
+		if ($scope.checkbox) {
 			var c =0;
 			for (var i = $scope.places.length - 1; i >= 0; i--) {
 				if ($scope.places[i].es_rapido == 1){
 					c ++;
-				}		
+				}
 			}
 			$scope.cantidad = c;
 		}
 		else{
-			$scope.cantidad = $scope.places.length;	
+			$scope.cantidad = $scope.places.length;
 		}
 
 	});
 
+	$scope.onChange = function () {
+	console.log($scope.cantidad);
+	}
+
+
 	$scope.esRapido = function () {
 	return function (item) {
-	  if ( $scope.checkbox == true ) {
-	  	if (item.es_rapido == 1){
-	    	return item;
-	  	}
-	  }
-	  if ( $scope.checkbox == false ) {
-	  	return item;	
-	  }
+		if ( $scope.checkbox == true ) {
+			if (item.es_rapido == 1){
+				return item;
+			}
+		}
+		if ( $scope.checkbox == false ) {
+			return item;
+		}
 	}
 	};
 	var onLocationError = function(e){
@@ -50,6 +55,7 @@ dondev2App.controller('locateListController',
   	}
   	$scope.nextShowUp =function(item){
 		$rootScope.places = $scope.places;
+		$scope.cantidad = $scope.places.length;
 	    $rootScope.currentMarker = item;
 	           $rootScope.centerMarkers = [];
 	      //tengo que mostrar arriba en el map si es dekstop.
@@ -62,7 +68,7 @@ dondev2App.controller('locateListController',
 
   		$scope.$apply(function(){
 
-	    	placesFactory.forLocation(position.coords, function(result){ 
+	    	placesFactory.forLocation(position.coords, function(result){
 	    	//console.log(result[0].condones);
 	    	var jsonObj= {
 	    		code: "all"
@@ -78,13 +84,13 @@ dondev2App.controller('locateListController',
 	    	}
 
 	var resultTemp = [];
-	    		    	
+
 		if(jsonObj.code ==="all"){
 			resultTemp = result;
 		}
 		else {
 
-			if (jsonObj.code == "condones"){ //codigo =  condon	
+			if (jsonObj.code == "condones"){ //codigo =  condon
 	    		console.log('entro en condon');
 		    	for (var i = 0; i < result.length ; i++) {
 		    		if (result[i].condones == 1)
@@ -92,21 +98,21 @@ dondev2App.controller('locateListController',
 		    	}
 			}
 
-	    	if (jsonObj.code == "vacunatorio"){ //codigo =  vacunacion 
+	    	if (jsonObj.code == "vacunatorio"){ //codigo =  vacunacion
 		    	console.log('entro en vacunatorio');
 					for (var i = 0; i < result.length ; i++) {
 		    		if (result[i].vacunatorio== 1)
 		    		resultTemp.push(result[i]);
 		    	}
 			}
-		    	
+
 	    	if (jsonObj.code == "prueba"){ //codigo =  prueba
 		    console.log('entro en prueba');
 		    	for (var i = 0; i < result.length ; i++) {
 		    		if (result[i].prueba== 1)
 		    		resultTemp.push(result[i]);
 		    	}
-			}	    
+			}
 
 	    	if (jsonObj.code == "infectologia"){ //codigo =  infectologia
 		    	console.log('entro en infectorlia');
@@ -114,11 +120,11 @@ dondev2App.controller('locateListController',
 		    		if (result[i].infectologia== 1)
 		    		resultTemp.push(result[i]);
 		    	}
-			}	
+			}
 		}
     	console.log(resultTemp);
-		
-	    				    		   
+
+
                 //$rootScope.places = $scope.places = $scope.closer = result;
 	          $rootScope.places = $scope.places = $scope.closer = resultTemp;
 	          $scope.cantidad = $scope.places.length;
