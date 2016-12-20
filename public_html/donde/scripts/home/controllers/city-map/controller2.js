c32b367cdecb2fdc934fd78337de1d2d967fef5b
jona_872 <jona_872@hotmail.com>
2016-12-20 15:13:29 -0300

    sinc

--- public_html/donde/scripts/home/controllers/city-map/controller2.js
+++ public_html/donde/scripts/home/controllers/city-map/controller2.js
@@ -3,78 +3,75 @@ dondev2App.controller('cityMapController2',
 
 var id = $routeParams.id;
 var urlShow ="api/v1/panel/places/"+id; 
-$scope.currentMarker = "calle";
-
+$scope.currentMarker = [];
+var resu = [];
 $http({
         method : "GET",
         url : urlShow
     }).then(function mySucces(response) {
-    	console.log(response.data)
-        $scope.currentMarker = response.data;
+    	// console.log(response.data[0].barrio_localidad)
+        $scope.currentMarker = response.data[0];
     }, function myError(response) {
-    	console.log('error en peticion')
+    	// console.log('error en peticion')
         // $scope.myWelcome = response.statusText;
     });
 
-
-	$rootScope.main = false;
-	$rootScope.geo = false;
-	$scope.province = $routeParams.provincia.split('-')[1];
-		$scope.provinceId = $routeParams.provincia.split('-')[0];
-		$scope.city = $routeParams.ciudad.split('-')[1];
-		$scope.cityId = $routeParams.ciudad.split('-')[0];
-		$scope.country = $routeParams.pais.split('-')[1];
-		$scope.countryId = $routeParams.pais.split('-')[0];
+	// $scope.province = $routeParams.provincia.split('-')[1];
+	// 	$scope.provinceId = $routeParams.provincia.split('-')[0];
+	// 	$scope.city = $routeParams.ciudad.split('-')[1];
+	// 	$scope.cityId = $routeParams.ciudad.split('-')[0];
+	// 	$scope.country = $routeParams.pais.split('-')[1];
+	// 	$scope.countryId = $routeParams.pais.split('-')[0];
 		
-		$scope.service = copyService.getFor($routeParams.servicio);
-		$rootScope.navBar =$scope.service ;
-		var search = {
-			provincia:	$scope.provinceId,
-			partido:	$scope.cityId,
-			pais: $scope.countryId,
-			service: $routeParams.servicio.toLowerCase(),
+	// 	$scope.service = copyService.getFor($routeParams.servicio);
+	// 	$rootScope.navBar =$scope.service ;
+	// 	var search = {
+	// 		provincia:	$scope.provinceId,
+	// 		partido:	$scope.cityId,
+	// 		pais: $scope.countryId,
+	// 		service: $routeParams.servicio.toLowerCase(),
 			
-		};
-		search[$routeParams.servicio.toLowerCase()] = true;
+	// 	};
+	// 	search[$routeParams.servicio.toLowerCase()] = true;
 		
-	$scope.showCurrent = function(i,p){
-      $rootScope.navBar = p.establecimiento;
-      $scope.currentMarker = p;
-    }
-    $scope.closeCurrent = function(){
-      $scope.currentMarker = undefined;
-    }
+	// $scope.showCurrent = function(i,p){
+ //      $rootScope.navBar = p.establecimiento;
+ //      $scope.currentMarker = p;
+ //    }
+ //    $scope.closeCurrent = function(){
+ //      $scope.currentMarker = undefined;
+ //    }
 
-    if ($rootScope.places.length > 0 && $rootScope.currentMarker){
-    	console.log($rootScope.currentMarker);
-    	       $rootScope.centerMarkers = [];
-      //tengo que mostrar arriba en el map si es dekstop.
-      $rootScope.centerMarkers.push($rootScope.currentMarker);
+ //    if ($rootScope.places.length > 0 && $rootScope.currentMarker){
+ //    	console.log($rootScope.currentMarker);
+ //    	       $rootScope.centerMarkers = [];
+ //      //tengo que mostrar arriba en el map si es dekstop.
+ //      $rootScope.centerMarkers.push($rootScope.currentMarker);
 
-    	$rootScope.moveMapTo = {
-			latitude:parseFloat($rootScope.currentMarker.latitude),
-			longitude:parseFloat($rootScope.currentMarker.longitude),
-			zoom:18,
-			center: true,
-		};
-    }else {
-		placesFactory.getAllFor(search, function(data){
-			$rootScope.places = $scope.places = data;
-			$rootScope.currentMarker = $scope.currentMarker = $scope.places[0];
-			console.log($rootScope.currentMarker);
-			$rootScope.moveMapTo = {
-				latitude:$rootScope.currentMarker.latitude,
-				longitude:$rootScope.currentMarker.longitude,
-				zoom:14,
-				center: true,
-			};
-			       $rootScope.centerMarkers = [];
-		      //tengo que mostrar arriba en el map si es dekstop.
-		      $rootScope.centerMarkers.push($rootScope.currentMarker);
+ //    	$rootScope.moveMapTo = {
+	// 		latitude:parseFloat($rootScope.currentMarker.latitude),
+	// 		longitude:parseFloat($rootScope.currentMarker.longitude),
+	// 		zoom:18,
+	// 		center: true,
+	// 	};
+ //    }else {
+	// 	placesFactory.getAllFor(search, function(data){
+	// 		$rootScope.places = $scope.places = data;
+	// 		$rootScope.currentMarker = $scope.currentMarker = $scope.places[0];
+	// 		console.log($rootScope.currentMarker);
+	// 		$rootScope.moveMapTo = {
+	// 			latitude:$rootScope.currentMarker.latitude,
+	// 			longitude:$rootScope.currentMarker.longitude,
+	// 			zoom:14,
+	// 			center: true,
+	// 		};
+	// 		       $rootScope.centerMarkers = [];
+	// 	      //tengo que mostrar arriba en el map si es dekstop.
+	// 	      $rootScope.centerMarkers.push($rootScope.currentMarker);
 
 			
-		})
-	}
+	// 	})
+	// }
 
 
 
