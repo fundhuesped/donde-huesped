b4e1bd8d5b6f7f8f38f2f08179d414928358936d
jona_872 <jona_872@hotmail.com>
2016-12-24 11:21:02 -0300

    change url share

--- public_html/donde/scripts/home/controllers/city-map/controller2.js
+++ public_html/donde/scripts/home/controllers/city-map/controller2.js
@@ -1,12 +1,6 @@
 dondev2App.controller('cityMapController2', 
-	// function($rootScope, $routeParams,$scope, $http){
 	function(placesFactory,NgMap, copyService, $scope,$rootScope, $routeParams, $location, $http){
 
-// $rootScope.$watch('currentMarker',function(){
-// 		 $scope.currentMarker = $rootScope.currentMarker;
-// 	})
-
-	
 		var id = $routeParams.id;
 		var urlShow ="api/v1/panel/places/"+id; 
 
@@ -14,77 +8,34 @@ dondev2App.controller('cityMapController2',
 			method : "GET",
 			url : urlShow
 		}).then(function mySucces(response) {
-			// $rootScope.centerMarkers = [];
-			console.log(response[0])
-	
 
-    $rootScope.main = false;
-	$rootScope.geo = false;
-	$scope.province = response.data[0].nombre_provincia;
-		$scope.provinceId = response.data[0].idProvincia;
-		$scope.city = response.data[0].nombre_partido;
-		$scope.cityId = response.data[0].idPartido;
-		$scope.country = response.data[0].nombre_pais;
-		$scope.countryId = response.data[0].idPais;
-		
-	$scope.showCurrent = function(i,p){
-      $scope.currentMarker = p;
-    }
+			$rootScope.main = false;
+			$rootScope.geo = false;
+			$scope.province = response.data[0].nombre_provincia;
+			$scope.provinceId = response.data[0].idProvincia;
+			$scope.city = response.data[0].nombre_partido;
+			$scope.cityId = response.data[0].idPartido;
+			$scope.country = response.data[0].nombre_pais;
+			$scope.countryId = response.data[0].idPais;
 
-    $scope.closeCurrent = function(){
-      $scope.currentMarker = undefined;
-    }
-		console.log('Entro en el else')
-		$rootScope.places = [response.data[0]];
-		$rootScope.currentMarker = response.data[0];
-		$scope.currentMarker = response.data[0];
-		// console.log($rootScope.currentMarker);
-		$rootScope.moveMapTo = {
-			latitude:parseFloat($rootScope.currentMarker.latitude),
-			longitude:parseFloat($rootScope.currentMarker.longitude),
-			zoom:18,
-			center: true,
-		};
-		  $rootScope.centerMarkers = [];
-	      //tengo que mostrar arriba en el map si es dekstop.
-	      $rootScope.centerMarkers.push($rootScope.currentMarker);
+			$scope.showCurrent = function(i,p){
+				$scope.currentMarker = p;
+			}
 
-			// $rootScope.currentMarker = response.data[0];
-			// console.log($rootScope.currentMarker);
-								
-			// $rootScope.centerMarkers = [];
+			$scope.closeCurrent = function(){
+				$scope.currentMarker = undefined;
+			}
+			$rootScope.places = [response.data[0]];
+			$rootScope.currentMarker = response.data[0];
+			$scope.currentMarker = response.data[0];
 			
-			// $rootScope.moveMapTo = {
-			// 	latitude:parseFloat($rootScope.currentMarker.latitude),
-			// 	longitude:parseFloat($rootScope.currentMarker.longitude),
-			// 	zoom: 8,
-			// 	center: true,
-			// };
-
-			// $rootScope.centerMarkers.push($rootScope.currentMarker);
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+			$rootScope.moveMapTo = {
+				latitude:parseFloat($rootScope.currentMarker.latitude),
+				longitude:parseFloat($rootScope.currentMarker.longitude),
+				zoom:18,
+				center: true,
+			};
+			$rootScope.centerMarkers = [];
+			$rootScope.centerMarkers.push($rootScope.currentMarker);
 		}); //del get
-
-
-
-
 	});
