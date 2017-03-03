95b5a66cfd850710d99a7e498ea67f4aca787c26
jona_872 <jona_872@hotmail.com>
2017-03-02 23:33:03 -0300

    sinc

--- donde-laravel-server/app/Http/routes.php
+++ donde-laravel-server/app/Http/routes.php
@@ -9,8 +9,17 @@ Route::get('/test', function () {
 
 Route::get('/phpHelp', function () {
 	return view("test"); });
-Route::get('/voted', function () {
-	return 22; });
+// Route::get('/voted', function () {
+// 	return 22; });
+
+
+Route::get('/view1', function () {
+	return view('panel.importer.confirmFast-id'); 
+});
+
+Route::get('/view2', function () {
+	return view('panel.importer.results-id'); 
+});
 
 
 
