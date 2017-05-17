a23fa1e3fc0deecbe52d5f133a1b5caaa21604f3
jona_872 <jona_872@hotmail.com>
2017-05-17 19:46:41 -0300

    optimization google responses

--- donde-laravel-server/app/Http/Controllers/ImportadorController.php
+++ donde-laravel-server/app/Http/Controllers/ImportadorController.php
@@ -1998,6 +1998,18 @@ public function geocode($book){
 
 }
 
+function curl_get_contents($url)
+{
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+  $data = curl_exec($ch);
+  curl_close($ch);
+  return $data;
+}
+
 public function geocodeExtra($book){
 	// dd( (isset($book->partido_comuna)) && (is_null($book->partido_comuna)) );
 	$address = "";
@@ -2033,13 +2045,41 @@ public function geocodeExtra($book){
 	// $url = "https://maps.googleapis.com.ar/maps/api/geocode/json?address={$address}&key=AIzaSyBoXKGMHwhiMfdCqGsa6BPBuX43L-2Fwqs";
 
 	// $url = "https://maps.google.com.ar/maps/api/geocode/json?address={$address}";
-	$url = "https://maps.google.com.ar/maps/api/geocode/json?address={$address}&key=AIzaSyBoXKGMHwhiMfdCqGsa6BPBuX43L-2Fwqs";
-		// get the json response
-		$resp_json = file_get_contents($url);
-	// dd($resp_json);
+	// dd($address);
+		// $url = "https://maps.google.com.ar/maps/api/geocode/json?key=AIzaSyBoXKGMHwhiMfdCqGsa6BPBuX43L-2Fwqs&address={$address}";
+
+	//$json = json_decode(curl_get_contents($address));
+
+		// $url = "https://maps.google.com.ar/maps/api/geocode/json?key=AIzaSyBoXKGMHwhiMfdCqGsa6BPBuX43L-2Fwqs&address={$address}";
+		// // get the json response
+
+		$url = "http://maps.google.com/maps/api/geocode/json?address=$address&sensor=false";
+		// $resp_json = file_get_contents($url);
+
+
+
+
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_PROXYPORT, 3128);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+$response = curl_exec($ch);
+curl_close($ch);
+
+$resp = json_decode($response,true);
+$location = json_decode($response);
+
 	    // decode the json
-	    $resp = json_decode($resp_json, true);
-	    $location = json_decode($resp_json);
+	    // $resp = json_decode($resp_json, true);
+	    // $location = json_decode($resp_json);
+
+// $resp = $resp_json;
+// $location =$resp_json;
+// dd($resp);
+
 
 	// dd($location);
 	if($resp['status']=='OK'){
