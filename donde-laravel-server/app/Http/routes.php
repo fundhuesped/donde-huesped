e9203de84062995b6cc3f5056660a33656d84497
Nahuel <nahueljaume@gmail.com>
2017-05-22 20:48:03 -0300

    route fix

--- donde-laravel-server/app/Http/routes.php
+++ donde-laravel-server/app/Http/routes.php
@@ -10,9 +10,9 @@ Route::get('/test', function () {
 Route::get('/phpHelp', function () {
 	return view("test"); });
 
-	Route::get('/contador', function () {
+/*	Route::get('/contador', function () {
 		return view("contador"); });
-
+*/
 Route::get('api/v2/evaluacion/getallquestionsresponses', 'QuestionController@getAllQuestionsResponses'); //Obtiene todas las preguntas y respuestas para evaluacion
 Route::get('api/v2/service/getAllServices', 'ServiceController@getAllServices');
 Route::get('api/v2/service/getPlaceServices/{placeId}', 'ServiceController@getPlaceServices');
