fb5ecf949171054d2267903ec81859cac23a3fd8
Nahuel <nahueljaume@gmail.com>
2017-07-04 18:29:33 -0300

    fix tag export

--- donde-laravel-server/app/Http/Controllers/PlaceLogController.php
+++ donde-laravel-server/app/Http/Controllers/PlaceLogController.php
@@ -45,6 +45,121 @@ class PlaceLogController extends Controller {
 		return $string;
 	}
 
+	public function insertArraObejectsDataIntoCsv_places($data){
+
+		$csv = Writer::createFromFileObject(new SplTempFileObject());
+		//header
+				//$csv->insertOne('id,establecimiento,tipo,calle,altura,piso_dpto,cruce,barrio_localidad,partido_comuna,provincia_region,pais,aprobado,observacion,formattedAddress,latitude,longitude,habilitado,confidence,condones,prueba,vacunatorio,infectologia,mac,ile,es_rapido,tel_testeo,mail_testeo,horario_testeo,responsable_testeo,web_testeo,ubicacion_testeo,observaciones_testeo,tel_distrib,mail_distrib,horario_distrib,responsable_distrib,web_distrib,ubicacion_distrib,comentarios_distrib,tel_infectologia,mail_infectologia,horario_infectologia,responsable_infectologia,web_infectologia,ubicacion_infectologia,comentarios_infectologia,tel_vac,mail_vac,horario_vac,responsable_vac,web_vac,ubicacion_vac,comentarios_vac,tel_mac,mail_mac,horario_mac,responsable_mac,web_mac,ubicacion_mac,comentarios_mac,tel_ile,mail_ile,horario_ile,responsable_ile,web_ile,ubicacion_ile,comentarios_ile');
+				$csv->insertOne($this->csvColumns);
+				//body
+				foreach ($data as $key => $p) {
+					$p->condones = $this->parseToExport($p->condones);
+					$p->prueba= $this->parseToExport($p->prueba);
+			//  	$p['vacunatorio']= $this->parseToExport($p['vacunatorio']);
+			//  	$p['infectologia']= $this->parseToExport($p['infectologia']);
+					$p->mac= $this->parseToExport($p->mac);
+					$p->ile = $this->parseToExport($p->ile);
+					$p->ssr = $this->parseToExport($p->ssr);
+					$p->dc= $this->parseToExport($p->dc);
+					$p->es_rapido= $this->parseToExport($p->es_rapido);
+					$csv->insertOne([
+						$p->placeId,
+						$p->establecimiento,
+						$p->tipo,
+						$p->calle,
+						$p->altura,
+				$p->piso_dpto,
+				$p->cruce,
+				$p->barrio_localidad,
+				//$p->partido_comuna,
+				$p->nombre_partido,
+				//$p->provincia_region,
+				$p->nombre_provincia,
+				$p->nombre_pais,
+				$p->aprobado,
+				$p->observacion,
+				$p->formattedAddress,
+				$p->latitude,
+				$p->longitude,
+				$p->habilitado,
+				$p->confidence,
+				$p->condones,
+				$p->prueba,
+		//		$p['vacunatorio'],
+			//	$p['infectologia'],
+				$p->mac,
+				$p->ile,
+				$p->ssr,
+				$p->dc,
+				$p->es_rapido,
+				$p->tel_testeo,
+				$p->mail_testeo,
+				$p->horario_testeo,
+				$p->responsable_testeo,
+				$p->web_testeo,
+				$p->ubicacion_testeo,
+				$p->observaciones_testeo,
+				$p->tel_distrib,
+				$p->mail_distrib,
+				$p->horario_distrib,
+				$p->responsable_distrib,
+				$p->web_distrib,
+				$p->ubicacion_distrib,
+				$p->comentarios_distrib,
+			/*	$p['tel_infectologia'],
+				$p['mail_infectologia'],
+				$p['horario_infectologia'],
+				$p['responsable_infectologia'],
+				$p['web_infectologia'],
+				$p['ubicacion_infectologia'],
+				$p['comentarios_infectologia'],
+				$p['tel_vac'],
+				$p['mail_vac'],
+				$p['horario_vac'],
+				$p['responsable_vac'],
+				$p['web_vac'],
+				$p['ubicacion_vac'],
+				$p['comentarios_vac'],*/
+				$p->tel_mac,
+				$p->mail_mac,
+				$p->horario_mac,
+				$p->responsable_mac,
+				$p->web_mac,
+				$p->ubicacion_mac,
+				$p->comentarios_mac,
+				$p->tel_ile,
+				$p->mail_ile,
+				$p->horario_ile,
+				$p->responsable_ile,
+				$p->web_ile,
+				$p->ubicacion_ile,
+				$p->comentarios_ile,
+				$p->tel_ssr,
+					$p->mail_ssr,
+					$p->horario_ssr,
+					$p->responsable_ssr,
+					$p->web_ssr,
+					$p->ubicacion_ssr,
+					$p->comentarios_ssr,
+					$p->tel_dc,
+					$p->mail_dc,
+					$p->horario_dc,
+					$p->responsable_dc,
+					$p->web_dc,
+					$p->ubicacion_dc,
+					$p->comentarios_dc,
+					strtolower($p->servicetype_ile),
+					strtolower($p->servicetype_mac),
+					strtolower($p->servicetype_condones),
+					strtolower($p->servicetype_prueba),
+					strtolower($p->servicetype_ssr),
+					strtolower($p->servicetype_dc)
+			]);
+				}
+				//descarga
+		return $csv;
+	}
+
 	public function insertDataIntoCsv_places($data){
 
 		$csv = Writer::createFromFileObject(new SplTempFileObject());
@@ -53,7 +168,6 @@ class PlaceLogController extends Controller {
 				$csv->insertOne($this->csvColumns);
         //body
         foreach ($data as $key => $p) {
-					$p=(array)$p;
         	$p['condones']= $this->parseToExport($p['condones']);
         	$p['prueba']= $this->parseToExport($p['prueba']);
       //  	$p['vacunatorio']= $this->parseToExport($p['vacunatorio']);
@@ -196,7 +310,7 @@ class PlaceLogController extends Controller {
 				else {
 					$csvName = "nodata.csv";
 				}
-				$csv= $this->insertDataIntoCsv_places($places);
+				$csv= $this->insertArraObejectsDataIntoCsv_places($places);
 				//descarga
 				$csv->output($csvName);
 			}
