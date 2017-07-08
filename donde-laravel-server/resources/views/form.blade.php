fb51f9389da1857c81360437c189fcee4020579f
Nahuel <nahueljaume@gmail.com>
2017-07-08 15:30:48 -0300

    front end form service type ok

--- donde-laravel-server/resources/views/form.blade.php
+++ donde-laravel-server/resources/views/form.blade.php
@@ -164,7 +164,9 @@
         <label for="Web">Web</label>
       </div>
     </div>
-    <p>
+
+
+    <div class="card-panel">
 
       <input  type="checkbox"
       name="place.condones"
@@ -172,8 +174,35 @@
       ng-checked="isChecked(place.condones)"
       ng-model="place.condones"/>
       <label for="filled-in-box-condones">¿Entrega condones?</label>
-    </p>
-    <p>
+
+      <div class="form-group" ng-show="place.condones">
+        <div class="col s12">
+          <label>Tipo de Servicio: </label>
+        </div>
+
+        <p>
+          <input type="radio" id="st_condones1" name="servicetype_condones" value="arancel" ng-model="place.servicetype_condones">
+          <label for="st_condones1">Arancel Diferencial</label>
+        </p>
+
+
+        <p>
+          <input type="radio" id="st_condones2" name="servicetype_condones" value="gratuito" ng-model="place.servicetype_condones">
+          <label for="st_condones2">Gratuito</label>
+        </p>
+
+
+        <p>
+          <input type="radio" id="st_condones3" name="servicetype_condones" value="cobertura" ng-model="place.servicetype_condones">
+          <label for="st_condones3">Consultar Cobertura</label>
+        </p>
+
+      </div>
+
+    </div>
+
+
+    <div class="card-panel">
 
       <input  type="checkbox"
       name="place.prueba"
@@ -181,43 +210,166 @@
       ng-checked="isChecked(place.prueba)"
       ng-model="place.prueba"/>
       <label for="filled-in-box-prueba" >¿Hace pruebas de HIV?</label>
-    </p>
 
-    <p>
+      <div class="form-group" ng-show="place.prueba">
+        <div class="col s12">
+          <label>Tipo de Servicio: </label>
+        </div>
+
+        <p>
+          <input type="radio" id="st_prueba1" name="servicetype_prueba" value="arancel" ng-model="place.servicetype_prueba">
+          <label for="st_prueba1">Arancel Diferencial</label>
+        </p>
+
+
+        <p>
+          <input type="radio" id="st_prueba2" name="servicetype_prueba" value="gratuito" ng-model="place.servicetype_prueba">
+          <label for="st_prueba2">Gratuito</label>
+        </p>
+
+
+        <p>
+          <input type="radio" id="st_prueba3" name="servicetype_prueba" value="cobertura" ng-model="place.servicetype_prueba">
+          <label for="st_prueba3">Consultar Cobertura</label>
+        </p>
+
+      </div>
+
+    </div>
+
+    <div class="card-panel">
       <input  type="checkbox"
       name="place.sd"
       id="filled-in-box-dc"
       ng-checked="isChecked(place.dc)"
       ng-model="place.dc"/>
       <label for="filled-in-box-dc">¿Cuenta con servicios de Detección de Cancer?</label>
-    </p>
 
-    <p>
+      <div class="form-group" ng-show="place.dc">
+        <div class="col s12">
+          <label>Tipo de Servicio: </label>
+        </div>
+
+        <p>
+          <input type="radio" id="st_dc1" name="servicetype_dc" value="arancel" ng-model="place.servicetype_dc">
+          <label for="st_dc1">Arancel Diferencial</label>
+        </p>
+
+
+        <p>
+          <input type="radio" id="st_dc2" name="servicetype_dc" value="gratuito" ng-model="place.servicetype_dc">
+          <label for="st_dc2">Gratuito</label>
+        </p>
+
+
+        <p>
+          <input type="radio" id="st_dc3" name="servicetype_dc" value="cobertura" ng-model="place.servicetype_dc">
+          <label for="st_dc3">Consultar Cobertura</label>
+        </p>
+
+      </div>
+    </div>
+
+    <div class="card-panel">
       <input  type="checkbox"
       name="place.ssr"
       id="filled-in-box-ssr"
       ng-checked="isChecked(place.ssr)"
       ng-model="place.ssr"/>
       <label for="filled-in-box-ssr">¿Cuenta con Servicios de Salud Sexual y Reproductiva?</label>
-    </p>
 
-    <p>
+      <div class="form-group" ng-show="place.ssr">
+        <div class="col s12">
+          <label>Tipo de Servicio: </label>
+        </div>
+
+        <p>
+          <input type="radio" id="st_ssr1" name="servicetype_ssr" value="arancel" ng-model="place.servicetype_ssr">
+          <label for="st_ssr1">Arancel Diferencial</label>
+        </p>
+
+
+        <p>
+          <input type="radio" id="st_ssr2" name="servicetype_ssr" value="gratuito" ng-model="place.servicetype_ssr">
+          <label for="st_ssr2">Gratuito</label>
+        </p>
+
+
+        <p>
+          <input type="radio" id="st_ssr3" name="servicetype_ssr" value="cobertura" ng-model="place.servicetype_ssr">
+          <label for="st_ssr3">Consultar Cobertura</label>
+        </p>
+
+      </div>
+
+    </div>
+    <div class="card-panel">
       <input  type="checkbox"
       name="place.mac"
       id="filled-in-box-mac"
       ng-checked="isChecked(place.mac)"
       ng-model="place.mac"/>
       <label for="filled-in-box-mac">¿Cuenta con servicios de Métodos Anticonceptivos?</label>
-    </p>
 
-    <p>
+      <div class="form-group" ng-show="place.mac">
+        <div class="col s12">
+          <label>Tipo de Servicio: </label>
+        </div>
+
+        <p>
+          <input type="radio" id="st_mac1" name="servicetype_mac" value="arancel" ng-model="place.servicetype_mac">
+          <label for="st_mac1">Arancel Diferencial</label>
+        </p>
+
+
+        <p>
+          <input type="radio" id="st_mac2" name="servicetype_mac" value="gratuito" ng-model="place.servicetype_mac">
+          <label for="st_mac2">Gratuito</label>
+        </p>
+
+
+        <p>
+          <input type="radio" id="st_mac3" name="servicetype_mac" value="cobertura" ng-model="place.servicetype_mac">
+          <label for="st_mac3">Consultar Cobertura</label>
+        </p>
+
+      </div>
+
+    </div>
+
+    <div class="card-panel">
       <input  type="checkbox"
       name="place.ile"
       id="filled-in-box-ile"
       ng-checked="isChecked(place.ile)"
       ng-model="place.ile"/>
       <label for="filled-in-box-ile">¿Da información sobre Interrupción legal del embarazo?</label>
-    </p>
+
+      <div class="form-group" ng-show="place.ile">
+        <div class="col s12">
+          <label>Tipo de Servicio: </label>
+        </div>
+
+        <p>
+          <input type="radio" id="st_ile1" name="servicetype_ile" value="arancel" ng-model="place.servicetype_ile">
+          <label for="st_ile1">Arancel Diferencial</label>
+        </p>
+
+
+        <p>
+          <input type="radio" id="st_ile2" name="servicetype_ile" value="gratuito" ng-model="place.servicetype_ile">
+          <label for="st_ile2">Gratuito</label>
+        </p>
+
+
+        <p>
+          <input type="radio" id="st_ile3" name="servicetype_ile" value="cobertura" ng-model="place.servicetype_ile">
+          <label for="st_ile3">Consultar Cobertura</label>
+        </p>
+
+      </div>
+
+    </div>
 
     <div class="row">
       <div class="input-field col s12">
@@ -248,40 +400,40 @@
                 <div class="circle-clipper left">
                   <div class="circle"></div>
                 </div><div class="gap-patch">
-                <div class="circle"></div>
-              </div><div class="circle-clipper right">
-              <div class="circle"></div>
+                  <div class="circle"></div>
+                </div><div class="circle-clipper right">
+                  <div class="circle"></div>
+                </div>
+              </div>
             </div>
-          </div>
+
+            <div class="" ng-cloak >
+              <i class="mdi-content-save left"></i>
+              Localizar con GPS
+            </div>
+
+          </button>
         </div>
+      </div>
+      <label>Ubicación</label>
 
-        <div class="" ng-cloak >
-          <i class="mdi-content-save left"></i>
-          Localizar con GPS
+      <input id="latitude" readonly type="text" name="latitude"
+      class="validate" ng-model="place.latitude" ng-change="onLatLonInputChange()">
+      <input id="longitude" readonly  type="text" name="longitude" class="validate" ng-model="place.longitude" ng-change="onLatLonInputChange()">
+      <div ng-cloak ng-show="waitingLocation">
+        <div class="progress">
+          <div class="indeterminate"></div>
         </div>
+      </div>
 
-      </button>
-    </div>
-  </div>
-  <label>Ubicación</label>
-
-  <input id="latitude" readonly type="text" name="latitude"
-  class="validate" ng-model="place.latitude" ng-change="onLatLonInputChange()">
-  <input id="longitude" readonly  type="text" name="longitude" class="validate" ng-model="place.longitude" ng-change="onLatLonInputChange()">
-  <div ng-cloak ng-show="waitingLocation">
-   <div class="progress">
-    <div class="indeterminate"></div>
-  </div>
-</div>
-
-<ng-map id="mapEditor"
-lazy-init="true" zoom="14">
-<marker ng-repeat="pos in positions"
-position="[[pos.latitude]],[[pos.longitude]]"
-on-dragend="onDragEnd()"
-draggable="true">
-</marker>
-</ng-map>
+      <ng-map id="mapEditor"
+      lazy-init="true" zoom="14">
+      <marker ng-repeat="pos in positions"
+      position="[[pos.latitude]],[[pos.longitude]]"
+      on-dragend="onDragEnd()"
+      draggable="true">
+    </marker>
+  </ng-map>
 
 
 </br>
@@ -297,16 +449,16 @@ draggable="true">
 
   <div class="input-field col s12">
 
-   <p>
-    <input type="checkbox" name="acepta_terminos"
-    class="filled-in" id="terminosCheck"
-    ng-change="formChange()"
-    ng-model="aceptaTerminos"/>
-    <label for="terminosCheck">Acepto los
-      <a href="/acerca" target="_blank">Términos y Condiciones</a> y la publicación de los datos en el sitio.</label>
-    </p>
+    <p>
+      <input type="checkbox" name="acepta_terminos"
+      class="filled-in" id="terminosCheck"
+      ng-change="formChange()"
+      ng-model="aceptaTerminos"/>
+      <label for="terminosCheck">Acepto los
+        <a href="/acerca" target="_blank">Términos y Condiciones</a> y la publicación de los datos en el sitio.</label>
+      </p>
+    </div>
   </div>
-</div>
 </div>
 <p></p>
 <div class="row">
@@ -322,21 +474,21 @@ draggable="true">
             <div class="circle-clipper left">
               <div class="circle"></div>
             </div><div class="gap-patch">
-            <div class="circle"></div>
-          </div><div class="circle-clipper right">
-          <div class="circle"></div>
+              <div class="circle"></div>
+            </div><div class="circle-clipper right">
+              <div class="circle"></div>
+            </div>
+          </div>
         </div>
-      </div>
+
+
+        <div class="" ng-cloak ng-show="!spinerflag">
+          Enviar
+        </div>
+
+      </button>
     </div>
-
-
-    <div class="" ng-cloak ng-show="!spinerflag">
-      Enviar
-    </div>
-
-  </button>
-</div>
-</div>
+  </div>
 </div>
 
 
@@ -349,9 +501,11 @@ draggable="true">
 </div>
 
 </div>
+
+
 <footer class="landing-service-footer">
-        <p>Donde es una <b>plataforma colaborativa.</b> Si encontrarás un error en los datos o en el funcionamiento de la plataforma <a href="mailto:donde@huesped.org.ar"  target="_self">envianos un mensaje</a></p>
-    </footer>
+  <p>Donde es una <b>plataforma colaborativa.</b> Si encontrarás un error en los datos o en el funcionamiento de la plataforma <a href="mailto:donde@huesped.org.ar"  target="_self">envianos un mensaje</a></p>
+</footer>
 </div>
 
 @stop
