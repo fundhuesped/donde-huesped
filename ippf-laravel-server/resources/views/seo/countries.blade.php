1bd26bdd29a824bbbf53472275c993d3f070ce8a
martin rabaglia <tincho@genosha.com.ar>
2017-09-21 22:59:22 -0300

    mewjoras paises

--- ippf-laravel-server/resources/views/seo/countries.blade.php
+++ ippf-laravel-server/resources/views/seo/countries.blade.php
@@ -58,33 +58,12 @@
       </ul>
     </div>
   </nav>
-<div class="boxLanding">
-	<ul class="collection ">
-		<!-- Header Tabble -->
-		<li class="collection-item collection-seo">
-			<div class="row valign">
-				<div class="row left-align">
-					<i class="mdi-hardware-keyboard-arrow-down i-seo"></i> <span class="distanceLanding"><b>@lang('site.select_country_2')</b></span>
-				</div>
-			</div>
-		</li>
-		<div class="countryLanding">
-			<div class="container">
-
-				<table class="highlight left">
-					<tbody>
-						@foreach ($countries as $c)
-						<tr>
-							<td><a class="item-seo" href="pais/{{$c->nombre_pais}}/provincia">{{$c->nombre_pais}} </a></td>
-						</tr>
-						@endforeach
-
-					</tbody>
-				</table>
-			</div>
-		</div>
-	</ul>
+<div ng-app="dondeDataVizApp">
+    <div class="container"> 
+        <div ng-view></div>
+    </div>
 </div>
+
 
 
 
@@ -95,10 +74,41 @@
 @stop
 
 @section('js')
-  <script
-  src="https://www.google.com/recaptcha/api.js?hl=es-419&onload=vcRecaptchaApiLoaded&render=explicit"
-  async defer
-></script>
-  {!!Html::script('bower_components/materialize/dist/js/materialize.min.js')!!}
-  {!!Html::script('bower_components/ngmap/build/scripts/ng-map.min.js')!!}
+ 
+
+    <!-- build:js(.) scripts/vendor.js -->
+    <!-- bower:js -->
+    <script src="/resume/bower_components/jquery/dist/jquery.js"></script>
+    <script src="/resume/bower_components/underscore/underscore-min.js"></script>
+    <script src="/resume/bower_components/materialize/bin/materialize.js"></script>
+    <script src="/resume/bower_components/moment/min/moment-with-locales.min.js"></script>
+    <script src="/resume/bower_components/angular/angular.min.js"></script>
+    <script src="/resume/bower_components/angular-materialize/src/angular-materialize.js"></script>
+    <script src="/resume/bower_components/angular-route/angular-route.min.js"></script>
+    <script src="/resume/bower_components/angular-sanitize/angular-sanitize.min.js"></script>
+    <script src="/resume/bower_components/angular-cookies/angular-cookies.min.js"></script>
+    <script src="/resume/bower_components/wow.js/dist/wow.min.js"></script>
+    <script src="/resume/bower_components/angucomplete/angucomplete.js"></script>
+    <script src="https://rawgit.com/allenhwkim/angularjs-google-maps/master/build/scripts/ng-map.js"></script>
+    <script src="https://maps.google.com/maps/api/js?key=AIzaSyCjb5c-5XvzhvdMXCjIjNaK-Zdh-L_qVmM"></script>
+    <script src="/resume/bower_components/angular-translate/angular-translate.js"></script>
+    <script src="/scripts/translations/es.js"></script>
+    <script src="/scripts/translations/br.js"></script>
+    <script src="/scripts/translations/en.js"></script>
+  
+
+    <script src="/resume/bower_components/moment/moment.js"></script>
+    <script src="/resume/bower_components/angular-moment/angular-moment.js"></script>
+    <script src="/resume/bower_components/odometer/odometer.js"></script>
+    <script src="/resume/bower_components/angular-odometer-js/dist/angular-odometer.js"></script>
+    <script src="/resume/bower_components/angular-socialshare/angular-socialshare.js"></script>
+    <script src="/resume/bower_components/smooth-scroll/dist/js/smooth-scroll.min.js"></script>
+    
+    <!-- endbower -->
+    <!-- endbuild -->
+
+    <!-- build:js({.tmp,app}) scripts/scripts.js -->
+    <script src="/resume/scripts/app.js"></script>
+    <script src="/resume/scripts/controllers/home.js"></script>
+    <!-- endbuild -->
 @stop
