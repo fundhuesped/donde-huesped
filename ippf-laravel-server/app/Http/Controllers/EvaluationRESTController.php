dd306f959dba74f27150b8cb288963b15893bfd3
Gonzalo Vittor <gavittor@gmail.com>
2017-09-28 20:36:23 -0300

    modify controller

--- ippf-laravel-server/app/Http/Controllers/EvaluationRESTController.php
+++ ippf-laravel-server/app/Http/Controllers/EvaluationRESTController.php
@@ -401,9 +401,10 @@ foreach ($dataSet as $provincia) {
 		}
 
 			$request_params = $request->all();
-		$this->debug_to_console($request_params->name);
-		$this->debug_to_console($request_params->email);
-		$this->debug_to_console($request_params->tel);
+		$this->debug_to_console($request->name);
+		$this->debug_to_console($request->email);
+		$this->debug_to_console($request->tel);
+
       	$validator = Validator::make($request_params,$rules,$messages);
 
 		if ($validator->passes()){
