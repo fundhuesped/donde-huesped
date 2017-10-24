7edc6207f3a2f76dae5605d29bae7a552180ca46
Franco Rabaglia <francoo.hd@gmail.com>
2017-10-24 12:19:45 -0300

    Favicon.

--- ippf-laravel-server/resources/views/acerca.blade.php
+++ ippf-laravel-server/resources/views/acerca.blade.php
@@ -1,249 +1,187 @@
 <!-- Modal Structure -->
 <div id="modal1" class="modal modal-fixed-footer">
 	<div class="modal-content">
+	<a style="float: left; width: 50%" href='https://www.ippfwhr.org/' target="_blank">
+		<img src="images/logo_ippf.png" width="50%" />
+	</a>
+	<a style="float:right;width: 50%; text-align: right;" href='https://www.huesped.org.ar/' target="_blank">
+		<img src="images/logo_huesped.png" width="40%" />
+	</a>
+	<br />
+	<br />
+	<br />
+	<br />
 
-	<div>
-
-				<a style="float: left; width: 50%" href='https://www.ippfwhr.org/' target="_blank">
-					<img src="images/logo_ippf.png" width="50%" />
-				</a>
-
-				<a style="float:right;width: 50%; text-align: right;" href='https://www.huesped.org.ar/' target="_blank">
-					<img src="images/logo_huesped.png" width="40%" />
-				</a>
-
-	</div>
-
-			<br />
-			<br />
-			<br />
-			<br />
-
-	<h2 id="menu" translate="about_usefull_information"></h2>
-    	<ul>
-
-    		<li> <a href="#/anchor#vamos" translate="about_href_label"></a></li>
-			<li> <a href="#/anchor#sobre" translate="about_href_aboutfh"></a></li>
-    		<li> <a href="#/anchor#comoFunciona" translate="about_href_how_it_works"></a></li>
-
-    	</ul>
+	<h3 id="menu" translate="about_usefull_information"></h3>
+	<ul>
+		<li><a href="#/anchor#vamos" translate="about_href_label"></a></li>
+		<li><a href="#/anchor#sobre" translate="about_href_aboutfh"></a></li>
+		<li><a href="#/anchor#comoFunciona" translate="about_href_how_it_works"></a></li>
+	</ul>
 
 	<h3 class="newMenuAbout" id="vamos" translate="about_href_label"></h3>
-		<p translate="about_donde_description"></p>
-
-		<p translate="about_donde_description_2">
-			<ul>
-				<li class="newAboutDefault" translate="about_li_label_preservativos"></li>
-				<li class="newAboutDefault" translate="about_li_label_mac"></li>
-				<li class="newAboutDefault" translate="about_li_label_prueba"></li>
-				<li class="newAboutDefault" translate="about_li_label_dc"></li>
-				<li class="newAboutDefault" translate="about_li_label_ssr"></li>
-				<li class="newAboutDefault" translate="about_li_label_ile"></li>
-			</ul>
-		</p>
-		<p translate="about_donde_description_3"></p>
-
-		<h4 translate="about_title_share_donde"></h4>
-		<p>
-			<div class="row">
-				<div class="col s12 m4 center-align newAboutLiMargin">
-					<a class="btn-floating btn-large share-buttons"
-							socialshare
-							socialshare-provider="twitter"
-							socialshare-text="[['social_share_text_2' | translate]]"
-							socialshare-url="https://vamoslac.org">
-							<i class="fa fa-twitter"></i>
-						</a>
-				</div>
-
-				<div class="col s12 m4 center-align newAboutLiMargin">
-					<a class="btn-floating btn-large share-buttons"
-							socialshare
-							socialshare-provider="facebook"
-							socialshare-text="[['social_share_text_2' | translate]]"
-							socialshare-url="https://vamoslac.org">
-							<i class="fa fa-facebook-square"></i>
-						</a>
-				</div>
-
-				<div class="col s12 m4 center-align newAboutLiMargin">
-					<a class="hide-on-med-and-up btn-floating btn-large share-buttons"
-							socialshare
-							socialshare-provider="whatsapp"
-							socialshare-text="[['social_share_text_2' | translate]]"
-							socialshare-url="https://vamoslac.org"  >
-							<i class="fa fa-whatsapp"></i>
-						</a>
-				</div>
-
-			</div>
-
-		</p>
-
-			<p>
-				<div class="row">
-					<div class="col s12 m12 center-align ">
-						<a style="color:#ed2840" href="https://github.com/njaume/DONDE" target="_blank" rel="external" class="ui-link""><i class="fa fa-github-square fa-3x"></i></a>
-						<!--a href="https://github.com/njaume/DONDE" target="_blank" rel="external" class="ui-link" translate="about_href_goto_git"></a-->
-
-						<br />
-
-						<span translate="about_href_goto_git"></span>
-
-					</div>
-				</div>
-			</p>
-
-		<h4 class="newMenuAbout" id="sobre" translate="about_vamos_title"></h4>
-		<p translate="about_donde_description_4"></p>
-
-		<h4 class="newMenuAbout"  >IPPF/RHO</h4>
-			<p translate="about_ippf_description"></p>
-			<p translate="about_ippf_description_2"></p>
-			<p translate="about_ippf_description_3"></p>
-			<p translate="about_ippf_description_4"></p>
-
- <!--
+	<p translate="about_donde_description"></p>
+	<p translate="about_donde_description_2">
 		<ul>
-			<li class="newAbout"> <a href="https://www.rhsupplies.org/" rel="external" target="_blank" class="ui-link">Reproductive Health Supplies Coalition (RHSC)</a> a través del fondo Innovation Fund, obtenido en alianza con el <a href="http://www.cedes.org/" rel="external" target="_blank" class="ui-link">Centro de Estudios de Estado y Sociedad (CEDES).</a></li>
-
-			<li class="newAbout"> <a href="https://www.ippfwhr.org/es" rel="external" target="_blank" class="ui-link">Federación Internacional de Planificación de la Familia/Región del Hemisferio Occidental (IPPF/RHO).</a></li>
-
-			<li class="newAbout"> <a href="http://www.desarrollosocial.gob.ar/" rel="external" target="_blank" class="ui-link">Ministerio de Desarrollo Social de la Nación</a></li>
+			<li class="newAboutDefault" translate="about_li_label_preservativos"></li>
+			<li class="newAboutDefault" translate="about_li_label_mac"></li>
+			<li class="newAboutDefault" translate="about_li_label_prueba"></li>
+			<li class="newAboutDefault" translate="about_li_label_dc"></li>
+			<li class="newAboutDefault" translate="about_li_label_ssr"></li>
+			<li class="newAboutDefault" translate="about_li_label_ile"></li>
 		</ul>
-	-->
-		<p><b translate="about_title_follow"></b>
+	</p>
+	<p translate="about_donde_description_3"></p>
+	<h4 translate="about_title_share_donde"></h4>
+	<p>
+		<div class="row">
+			<div class="col s12 m4 center-align newAboutLiMargin">
+				<a class="btn-floating btn-large share-buttons"
+						socialshare
+						socialshare-provider="twitter"
+						socialshare-text="[['social_share_text_2' | translate]]"
+						socialshare-url="https://vamoslac.org">
+						<i class="fa fa-twitter"></i>
+					</a>
+			</div>
 
-				<div class="row">
+			<div class="col s12 m4 center-align newAboutLiMargin">
+				<a class="btn-floating btn-large share-buttons"
+						socialshare
+						socialshare-provider="facebook"
+						socialshare-text="[['social_share_text_2' | translate]]"
+						socialshare-url="https://vamoslac.org">
+						<i class="fa fa-facebook-square"></i>
+					</a>
+			</div>
 
-					<div class="col s12 m4 center-align newAboutLiMargin">
-						<a href="https://twitter.com/IPPF_WHR" rel="external" target="_blank" class="ui-link">
-	<i class="fa fa-twitter fa-4x"></i>Twitter</a>
-					</div>
+			<div class="col s12 m4 center-align newAboutLiMargin">
+				<a class="hide-on-med-and-up btn-floating btn-large share-buttons"
+						socialshare
+						socialshare-provider="whatsapp"
+						socialshare-text="[['social_share_text_2' | translate]]"
+						socialshare-url="https://vamoslac.org"  >
+						<i class="fa fa-whatsapp"></i>
+					</a>
+			</div>
+		</div>
+	</p>
 
-					<div class="col s12 m4 center-align newAboutLiMargin">
-						<a href="https://www.facebook.com/IPPF.WesternHemisphereRegion/" rel="external" target="_blank" class="ui-link">
-	<i class="fa fa-facebook fa-4x"></i>Facebook</a>
-					</div>
+	<p>
+		<div class="row">
+			<div class="col s12 m12 center-align ">
+				<a style="color:#ed2840" href="https://github.com/frabaglia/DONDE" target="_blank" rel="external" class="ui-link">
+					<i class="fa fa-github-square fa-3x"></i>
+				</a>
+				<br />
+				<span translate="about_href_goto_git"></span>
+			</div>
+		</div>
+	</p>
 
-					<div class="col s12 m4 center-align newAboutLiMargin">
-						<a href="https://www.instagram.com/ippf_whr/" rel="external" target="_blank" class="ui-link">
-		<i class="fa fa-instagram fa-4x"></i> Instagram</a>
-					</div>
+	<h3 class="newMenuAbout" id="sobre" translate="about_vamos_title"></h3>
+	<p translate="about_donde_description_4"></p>
 
-				</div>
-			</p>
-			<p><b translate="about_title_visit_page"></b>
-				<div class="row">
-					<div class="col s12 m12 center-align" style="margin: 0 auto;">
-						<a href="https://www.ippfwhr.org/" rel="external" target="_blank" class="ui-link">
-		<img class="newAboutIcon50" src="images/logo_ippf.png" title="IPPF"></a>
-					</div>
-				</div>
-			</p>
+	<h4 class="newMenuAbout">IPPF/RHO</h4>
+	<p translate="about_ippf_description"></p>
+	<p translate="about_ippf_description_2"></p>
+	<p translate="about_ippf_description_3"></p>
+	<p translate="about_ippf_description_4"></p>
 
-			<h3 id='fundacionHuesped'>Fundación Huésped</h3>
-			<p translate="about_fh_description"></p>
-			<p translate="about_fh_description_2"></p>
+	<p>
+		<h4 translate="about_title_follow"></h4>
+		<div class="row">
+			<div class="col s12 m4 center-align newAboutLiMargin">
+				<a href="https://twitter.com/IPPF_WHR" rel="external" target="_blank" class="ui-link">
+					<i class="fa fa-twitter fa-4x"></i>
+					Twitter
+				</a>
+			</div>
 
-			<p><b translate="about_title_follow"></b>
+			<div class="col s12 m4 center-align newAboutLiMargin">
+				<a href="https://www.facebook.com/IPPF.WesternHemisphereRegion/" rel="external" target="_blank" class="ui-link">
+					<i class="fa fa-facebook fa-4x"></i>
+					Facebook
+				</a>
+			</div>
 
-				<div class="row">
+			<div class="col s12 m4 center-align newAboutLiMargin">
+				<a href="https://www.instagram.com/ippf_whr/" rel="external" target="_blank" class="ui-link">
+					<i class="fa fa-instagram fa-4x"></i>
+					Instagram
+				</a>
+			</div>
+		</div>
+	</p>
 
-					<div class="col s12 m4 center-align newAboutLiMargin">
-						<a href="https://twitter.com/fundhuesped" rel="external" target="_blank" class="ui-link">
-	<i class="fa fa-twitter fa-4x"></i>Twitter</a>
-					</div>
-
-					<div class="col s12 m4 center-align newAboutLiMargin">
-						<a href="https://www.facebook.com/FundHuesped" rel="external" target="_blank" class="ui-link">
-	<i class="fa fa-facebook fa-4x"></i>Facebook</a>
-					</div>
-
-					<div class="col s12 m4 center-align newAboutLiMargin">
-						<a href="https://www.instagram.com/fundhuesped" rel="external" target="_blank" class="ui-link">
-		<i class="fa fa-instagram fa-4x"></i> Instagram</a>
-					</div>
-
-				</div>
-			</p>
-
-			<p><b translate="about_title_visit_page"></b>
-				<div class="row">
-					<div class="col s12 m12 center-align" style="margin: 0 auto;">
-						<a href="https://www.huesped.org.ar/" rel="external" target="_blank" class="ui-link">
-		<img class="newAboutIcon50" src="images/logo_huesped.png" title="IPPF"></a>
-					</div>
-				</div>
-			</p>
-
-				<h3 id="comoFunciona" translate="about_href_how_it_works"> </h3>
-				<h4 translate="about_title_find_services"> </h4>
-				<p translate="about_description_find_services"></p>
-
-				<h4 translate="about_title_evaluate_services"></h4>
-				<p translate="about_description_evaluate_services"></p>
-
-				<h4 translate="about_title_add_info">  </h4>
-
-			<p>
-				<span translate="about_descripcion1_add_info"></span>
-				<a href="https://vamoslac.org/form" rel="external" target="_blank" class="ui-link" translate="about_description2_add_info"></a>
-				<span translate="about_description3_add_info"></span>
-				<img class="newAboutIcon+" src="images/about/+new_btn.png" width="32px" title="IPPF">
-				<span translate="about_description4_add_info"></span>
-				<span translate="about_description5_add_info"></span>
-				<a href="mailto:vamoslacapp@gmail.com" rel="external" target="_blank" class="ui-link" translate="about_description6_add_info"></a>
-			</p>
-
-<!--
-		<p>
-			<div class="row">
-				<div class="col s6 center-align newAboutLiMargin">
-					 <a href="https://www.rhsupplies.org/" rel="external" target="_blank" class="ui-link">
-					<img class="newAboutIcon40 " src="images/about/rhsc.png" alt="RHSC" title="RHSC"></a>
-				</div>
-				<div class="col s6 center-align newAboutLiMargin">
-					<a href="http://www.cedes.org/ " rel="external" target="_blank" class="ui-link">
-					<img class="newAboutIcon50" src="images/about/cedes.gif" alt="Cedes" title="Cedes"></a>
-				</div>
-				<div class="col s6 center-align newAboutLiMargin">
-					<a href="https://www.ippfwhr.org/es" rel="external" target="_blank" class="ui-link">
-					<img class="newAboutIcon30" src="images/about/IPPF.png" alt="Ippf" title="Ippf"></a>
-				</div>
-				<div class="col s6 center-align newAboutLiMargin">
-					<a href="http://www.desarrollosocial.gob.ar" rel="external" target="_blank" class="ui-link">
-					<img class="newAboutIcon40" src="images/about/ds.png" alt="Ministerio" title="Ministerio"></a>
+	<p>
+		<b translate="about_title_visit_page"></b>
+		<div class="row">
+			<div class="col s12 m12 center-align" style="margin: 0 auto;">
+				<a href="https://www.ippfwhr.org/" rel="external" target="_blank" class="ui-link">
+					<img class="newAboutIcon50" src="images/logo_ippf.png" title="IPPF"></a>
 				</div>
 			</div>
-		</p>
+	</p>
 
-		<p>
-			<a href="https://www.facebook.com/ColectivoDeJuventudes" rel="external" target="_blank" class="ui-link"> El Colectivo de Juventudes por los Derechos Sexuales y Reproductivos</a> constituye una alianza estratégica para esta iniciativa ya que en conjunto se trabajó en la validación de la plataforma y sus contenidos así como en la planificación e implementación de acciones de difusión.
-		</p>
+	<h3 id='fundacionHuesped'>Fundación Huésped</h3>
+	<p translate="about_fh_description"></p>
+	<p translate="about_fh_description_2"></p>
+	<p>
+		<h4 translate="about_title_follow"></h4>
+		<div class="row">
+			<div class="col s12 m4 center-align newAboutLiMargin">
+				<a href="https://twitter.com/fundhuesped" rel="external" target="_blank" class="ui-link">
+					<i class="fa fa-twitter fa-4x"></i>
+					Twitter
+				</a>
+			</div>
 
+			<div class="col s12 m4 center-align newAboutLiMargin">
+				<a href="https://www.facebook.com/FundHuesped" rel="external" target="_blank" class="ui-link">
+					<i class="fa fa-facebook fa-4x"></i>
+					Facebook
+				</a>
+			</div>
 
-	<h3 id="sobreFH"> Sobre Fundación Huésped </h3>
-	<p>Fundación Huésped es una organización argentina con alcance regional que, desde 1989, trabaja en áreas de salud pública con el objetivo de que el derecho a la salud y el control de enfermedades sean garantizados. A partir de un proceso de planificación estratégica en el que se amplió la visión institucional, trabajamos con foco en VIH/sida, Hepatitis virales, enfermedades prevenibles por vacunas y otras enfermedades transmisibles como dengue y zika, entre otras, así como en salud sexual y reproductiva.</p>
-	<p>Nuestro abordaje integral incluye el desarrollo de investigaciones y soluciones prácticas vinculadas a las políticas de salud pública en nuestro país y en la región. También realizamos acciones masivas de comunicación y prevención innovadoras y de alto impacto a través de una constante presencia en medios de comunicación y redes sociales.</p>
+			<div class="col s12 m4 center-align newAboutLiMargin">
+				<a href="https://www.instagram.com/fundhuesped" rel="external" target="_blank" class="ui-link">
+					<i class="fa fa-instagram fa-4x"></i>
+					Instagram
+				 </a>
+			</div>
+		</div>
+	</p>
 
+	<p>
+		<h4 translate="about_title_visit_page"></h4>
+		<div class="row">
+			<div class="col s12 m12 center-align" style="margin: 0 auto;">
+				<a href="https://www.huesped.org.ar/" rel="external" target="_blank" class="ui-link">
+					<img class="newAboutIcon50" src="images/logo_huesped.png" title="IPPF"></a>
+			</div>
+		</div>
+	</p>
 
+		<h3 id="comoFunciona" translate="about_href_how_it_works"> </h3>
+		<h4 translate="about_title_find_services"> </h4>
+		<p translate="about_description_find_services"></p>
 
-			<h3 id="origen"> Origen de los datos </h3>
-			<p>Los sitios incluidos en #Dónde se obtuvieron gracias a la colaboración de diferentes organismos y a través de pedidos de información.</a> Como #Dónde es una plataforma colaborativa, los datos se amplían y mejoran con los aportes de cada usuari@.</p>
-			<p>Agradecemos especialmente a:</p>
-			<ul>
-				<li>Ministerio de Salud de la Nación, Dirección de Control de Enfermedades Inmunoprevenibles (DiCEI), Sistema Integrado de Información Sanitaria de Argentina (SISA), Programa Nacional de Salud Integral en la Adolescencia (PNSIA), Dirección Nacional de Sida.</li>
-				<li>Gobierno de la Ciudad de Buenos Aires, Ministerio de Modernización, Dirección de Calidad Institucional de la Subsecretaría de Gestión Estratégica y Calidad Institucional, Coordinación Sida y Salud Sexual y Reproductiva. </li>
-				<li>Ministerio de Salud de la Provincia de Buenos Aires: Dirección de VIH, ITS y hepatitis virales, Dirección Provincial de Programas Sanitarios; Programa Provincial de Salud Sexual y Reproductiva, Subsecretaría de Atención de la Salud de las Personas.</li>
-				<li>Programas Provinciales de Salud Sexual y Procreación Responsable de Salta, Misiones y Santiago del Estero, Secretaría de Prevención y Promoción de la Salud de Córdoba, Sub Dirección de Políticas de Género e Interculturalidad en Salud de Santa Fe.</li>
-				<li>OMS en Argentina, ONUSIDA América Latina.</li>
-				<li>Alianza de organizaciones integrada por Amnistía Internacional Argentina, Grupo FUSA y Católicas por el Derecho a Decidir con apoyo de IPPF.</li>
-				<li>Colectivas de Socorristas en Red, Red de profesionales de la salud por el derecho a decidir, Colectivo de Salud Feminista - Comohacerseunaborto.com.</li>
-			</ul>
--->
+		<h4 translate="about_title_evaluate_services"></h4>
+		<p translate="about_description_evaluate_services"></p>
+
+		<h4 translate="about_title_add_info">  </h4>
+
+	<p>
+		<span translate="about_descripcion1_add_info"></span>
+		<a href="https://vamoslac.org/form" rel="external" target="_blank" class="ui-link" translate="about_description2_add_info"></a>
+		<span translate="about_description3_add_info"></span>
+		<img class="newAboutIcon+" src="images/about/+new_btn.png" width="32px" title="IPPF">
+		<span translate="about_description4_add_info"></span>
+		<span translate="about_description5_add_info"></span>
+		<a href="mailto:vamoslacapp@gmail.com" rel="external" target="_blank" class="ui-link" translate="about_description6_add_info"></a>
+	</p>
 	</div> {{-- modal content --}}
-
 	<div class="modal-footer">
 		<a href=""
 		class="modal-action modal-close btn-flat " translate="close_label_button"></a>
