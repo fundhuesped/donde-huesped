7663596b12f6dec10818db7f49dd391d348182de
Gonzalo Vittor <gavittor@gmail.com>
2017-10-25 03:04:24 -0300

    Add back buttons

--- ippf-laravel-server/resources/views/acerca.blade.php
+++ ippf-laravel-server/resources/views/acerca.blade.php
@@ -1,203 +1,204 @@
-<!-- Esta es el blade-->
-<div id="modal" class="modal modal-fixed-footer">
+<!-- Modal Structure -->
+<div id="modal1" class="modal modal-fixed-footer">
 	<div class="modal-content">
-	<a style="float: left; width: 50%" href='https://www.ippfwhr.org/' target="_blank">
-		<img src="images/logo_ippf.png" width="50%" />
-	</a>
-	<a style="float:right;width: 50%; text-align: right;" href='https://www.huesped.org.ar/' target="_blank">
-		<img src="images/logo_huesped.png" width="40%" />
-	</a>
 
-	<!-- START SECTION 1 -->
-	<h3 id="menu" translate="about_usefull_information"></h3>
-	<ul>
-		<li><a href="#/anchor#vamos" translate="about_href_label"></a></li>
-		<li><a href="#/anchor#sobre" translate="about_href_aboutfh"></a></li>
-		<li><a href="#/anchor#comoFunciona" translate="about_href_how_it_works"></a></li>
-	</ul>
+	<div>
+
+				<a style="float: left; width: 50%" href='https://www.ippfwhr.org/' target="_blank">
+					<img src="images/logo_ippf.png" width="50%" />
+				</a>
+
+				<a style="float:right;width: 50%; text-align: right;" href='https://www.huesped.org.ar/' target="_blank">
+					<img src="images/logo_huesped.png" width="40%" />
+				</a>
+
+	</div>
+
+			<br />
+			<br />
+			<br />
+			<br />
+
+	<h2 id="menu" translate="about_usefull_information"></h2>
+    	<ul>
+
+    		<li> <a href="#/anchor#vamos" translate="about_href_label"></a></li>
+			<li> <a href="#/anchor#sobre" translate="about_href_aboutfh"></a></li>
+    		<li> <a href="#/anchor#comoFunciona" translate="about_href_how_it_works"></a></li>
+
+    	</ul>
 
 	<h3 class="newMenuAbout" id="vamos" translate="about_href_label"></h3>
-	<p translate="about_donde_description"></p>
-	<p translate="about_donde_description_2">
-		<ul>
-			<li class="newAboutDefault" translate="about_li_label_preservativos"></li>
-			<li class="newAboutDefault" translate="about_li_label_mac"></li>
-			<li class="newAboutDefault" translate="about_li_label_prueba"></li>
-			<li class="newAboutDefault" translate="about_li_label_dc"></li>
-			<li class="newAboutDefault" translate="about_li_label_ssr"></li>
-			<li class="newAboutDefault" translate="about_li_label_ile"></li>
-		</ul>
-	</p>
-	<p translate="about_donde_description_3"></p>
-	<h4 translate="about_title_share_donde"></h4>
-	<p>
-		<div class="row">
-			<div class="col s12 m4 center-align newAboutLiMargin">
-				<a class="btn-floating btn-large share-buttons"
-						socialshare
-						socialshare-provider="twitter"
-						socialshare-text="[['social_share_text_2' | translate]]"
-						socialshare-url="https://vamoslac.org">
-						<i class="fa fa-twitter"></i>
-					</a>
-			</div>
+		<p translate="about_donde_description"></p>
 
-			<div class="col s12 m4 center-align newAboutLiMargin">
-				<a class="btn-floating btn-large share-buttons"
-						socialshare
-						socialshare-provider="facebook"
-						socialshare-text="[['social_share_text_2' | translate]]"
-						socialshare-url="https://vamoslac.org">
-						<i class="fa fa-facebook-square"></i>
-					</a>
-			</div>
+		<p translate="about_donde_description_2">
+			<ul>
+				<li class="newAboutDefault" translate="about_li_label_preservativos"></li>
+				<li class="newAboutDefault" translate="about_li_label_mac"></li>
+				<li class="newAboutDefault" translate="about_li_label_prueba"></li>
+				<li class="newAboutDefault" translate="about_li_label_dc"></li>
+				<li class="newAboutDefault" translate="about_li_label_ssr"></li>
+				<li class="newAboutDefault" translate="about_li_label_ile"></li>
+			</ul>
+		</p>
+		<p translate="about_donde_description_3"></p>
 
-			<div class="col s12 m4 center-align newAboutLiMargin">
-				<a class="hide-on-med-and-up btn-floating btn-large share-buttons"
-						socialshare
-						socialshare-provider="whatsapp"
-						socialshare-text="[['social_share_text_2' | translate]]"
-						socialshare-url="https://vamoslac.org"  >
-						<i class="fa fa-whatsapp"></i>
-					</a>
-			</div>
-		</div>
-	</p>
-
-	<h4 translate="about_href_goto_git"></h4>
-	<p>
-		<div class="row">
-			<div class="col s12 m12 center-align ">
-				<a style="color:#ed2840" href="https://github.com/frabaglia/DONDE" target="_blank" rel="external" class="ui-link">
-					<i class="fa fa-github-square fa-3x"></i>
-				</a>
-			</div>
-		</div>
-	</p>
-	<!-- END SECTION 1 -->
-
-	<!-- RETURN MENU -->
-	<a class="waves-effect waves-light btn back-button" href="#/anchor#menu" translate="back"></a>
-
-	<!-- START SECTION 2 -->
-	<h3 class="newMenuAbout" id="sobre" translate="about_vamos_title"></h3>
-	<p translate="about_donde_description_4"></p>
-
-	<h4 class="newMenuAbout">International Planned Parenthood Western Hemisphere Region</h4>
-	<p translate="about_ippf_description"></p>
-	<p translate="about_ippf_description_2"></p>
-	<p translate="about_ippf_description_3"></p>
-	<p translate="about_ippf_description_4"></p>
-
-	<p>
-		<h4 translate="about_title_follow"></h4>
-		<div class="row">
-			<div class="col s12 m4 center-align newAboutLiMargin">
-				<a href="https://twitter.com/IPPF_WHR" rel="external" target="_blank" class="ui-link">
-					<i class="fa fa-twitter fa-4x"></i>
-					Twitter
-				</a>
-			</div>
-
-			<div class="col s12 m4 center-align newAboutLiMargin">
-				<a href="https://www.facebook.com/IPPF.WesternHemisphereRegion/" rel="external" target="_blank" class="ui-link">
-					<i class="fa fa-facebook fa-4x"></i>
-					Facebook
-				</a>
-			</div>
-
-			<div class="col s12 m4 center-align newAboutLiMargin">
-				<a href="https://www.instagram.com/ippf_whr/" rel="external" target="_blank" class="ui-link">
-					<i class="fa fa-instagram fa-4x"></i>
-					Instagram
-				</a>
-			</div>
-		</div>
-	</p>
-
-	<p>
-		<h4 translate="about_title_visit_page"></h4>
-		<div class="row">
-			<div class="col s12 m12 center-align" style="margin: 0 auto;">
-				<a href="https://www.ippfwhr.org/" rel="external" target="_blank" class="ui-link">
-					<img class="newAboutIcon50" src="images/logo_ippf.png" title="IPPF"></a>
+		<h4 translate="about_title_share_donde"></h4>
+		<p>
+			<div class="row">
+				<div class="col s12 m4 center-align newAboutLiMargin">
+					<a class="btn-floating btn-large share-buttons"
+							socialshare
+							socialshare-provider="twitter"
+							socialshare-text="[['social_share_text_2' | translate]]"
+							socialshare-url="https://vamoslac.org">
+							<i class="fa fa-twitter"></i>
+						</a>
 				</div>
-			</div>
-	</p>
-	<!-- END SECTION 2 -->
 
-	<!-- RETURN MENU -->
-	<a class="waves-effect waves-light btn back-button" href="#/anchor#menu" translate="back"></a>
+				<div class="col s12 m4 center-align newAboutLiMargin">
+					<a class="btn-floating btn-large share-buttons"
+							socialshare
+							socialshare-provider="facebook"
+							socialshare-text="[['social_share_text_2' | translate]]"
+							socialshare-url="https://vamoslac.org">
+							<i class="fa fa-facebook-square"></i>
+						</a>
+				</div>
 
-	<!-- START SECTION 3 -->
-	<h3 id='fundacionHuesped'>Fundación Huésped</h3>
-	<p translate="about_fh_description"></p>
-	<p translate="about_fh_description_2"></p>
-	<p>
-		<h4 translate="about_title_follow"></h4>
-		<div class="row">
-			<div class="col s12 m4 center-align newAboutLiMargin">
-				<a href="https://twitter.com/fundhuesped" rel="external" target="_blank" class="ui-link">
-					<i class="fa fa-twitter fa-4x"></i>
-					Twitter
-				</a>
-			</div>
+				<div class="col s12 m4 center-align newAboutLiMargin">
+					<a class="hide-on-med-and-up btn-floating btn-large share-buttons"
+							socialshare
+							socialshare-provider="whatsapp"
+							socialshare-text="[['social_share_text_2' | translate]]"
+							socialshare-url="https://vamoslac.org"  >
+							<i class="fa fa-whatsapp"></i>
+						</a>
+				</div>
 
-			<div class="col s12 m4 center-align newAboutLiMargin">
-				<a href="https://www.facebook.com/FundHuesped" rel="external" target="_blank" class="ui-link">
-					<i class="fa fa-facebook fa-4x"></i>
-					Facebook
-				</a>
 			</div>
 
-			<div class="col s12 m4 center-align newAboutLiMargin">
-				<a href="https://www.instagram.com/fundhuesped" rel="external" target="_blank" class="ui-link">
-					<i class="fa fa-instagram fa-4x"></i>
-					Instagram
-				 </a>
-			</div>
-		</div>
-	</p>
+		</p>
 
-	<p>
-		<h4 translate="about_title_visit_page"></h4>
-		<div class="row">
-			<div class="col s12 m12 center-align" style="margin: 0 auto;">
-				<a href="https://www.huesped.org.ar/" rel="external" target="_blank" class="ui-link">
-					<img class="newAboutIcon50" src="images/logo_huesped.png" title="IPPF"></a>
-			</div>
-		</div>
-	</p>
-	<!-- END SECTION 3 -->
+			<p>
+				<div class="row">
+					<div class="col s12 m12 center-align ">
+						<a style="color:#ed2840" href="https://github.com/njaume/DONDE" target="_blank" rel="external" class="ui-link""><i class="fa fa-github-square fa-3x"></i></a>
+						<br />
 
-	<!-- RETURN MENU -->
-	<a class="waves-effect waves-light btn back-button" href="#/anchor#menu" translate="back"></a>
+						<span translate="about_href_goto_git"></span>
 
-	<!-- START SECTION 4 -->
-	<h3 id="comoFunciona" translate="about_href_how_it_works"> </h3>
+					</div>
+				</div>
+			</p>
 
-	<!-- HOW TO FIND SERVICES -->
-	<h4 translate="about_title_find_services"> </h4>
-	<p translate="about_description_find_services"></p>
+			<!-- RETURN MENU -->
+			<a class="waves-effect waves-light btn back-button" href="#/anchor#menu" translate="back"></a>
 
-	<!-- HOW TO EVALUATE SERVICES -->
-	<h4 translate="about_title_evaluate_services"></h4>
-	<p translate="about_description_evaluate_services"></p>
+		<h4 class="newMenuAbout" id="sobre" translate="about_vamos_title"></h4>
+		<p translate="about_donde_description_4"></p>
 
-	<!-- HOW TO ADD INFO -->
-	<h4 translate="about_title_add_info"></h4>
-	<span translate="about_descripcion1_add_info"></span>
-	<a href="https://vamoslac.org/form" rel="external" target="_blank" class="ui-link" translate="about_description2_add_info"></a>
-	<span translate="about_description3_add_info"></span>
-	<img class="newAboutIcon+" src="images/about/+new_btn.png" width="32px" title="IPPF">
-	<span translate="about_description4_add_info"></span>
-	<span translate="about_description5_add_info"></span>
-	<a href="mailto:vamoslacapp@gmail.com" rel="external" target="_blank" class="ui-link" translate="about_description6_add_info"></a>
-	<!-- END SECTION 4 -->
+		<h4 class="newMenuAbout">International Planned Parenthood Western Hemisphere Region</h4>
+			<p translate="about_ippf_description"></p>
+			<p translate="about_ippf_description_2"></p>
+			<p translate="about_ippf_description_3"></p>
+			<p translate="about_ippf_description_4"></p>
 
-	<!-- RETURN MENU -->
-	<a class="waves-effect waves-light btn back-button" href="#/anchor#menu" translate="back"></a>
-	</div>
+		<p><b translate="about_title_follow"></b>
+
+				<div class="row">
+
+					<div class="col s12 m4 center-align newAboutLiMargin">
+						<a href="https://twitter.com/IPPF_WHR" rel="external" target="_blank" class="ui-link">
+	<i class="fa fa-twitter fa-4x"></i>Twitter</a>
+					</div>
+
+					<div class="col s12 m4 center-align newAboutLiMargin">
+						<a href="https://www.facebook.com/IPPF.WesternHemisphereRegion/" rel="external" target="_blank" class="ui-link">
+	<i class="fa fa-facebook fa-4x"></i>Facebook</a>
+					</div>
+
+					<div class="col s12 m4 center-align newAboutLiMargin">
+						<a href="https://www.instagram.com/ippf_whr/" rel="external" target="_blank" class="ui-link">
+		<i class="fa fa-instagram fa-4x"></i> Instagram</a>
+					</div>
+
+				</div>
+			</p>
+			<p><b translate="about_title_visit_page"></b>
+				<div class="row">
+					<div class="col s12 m12 center-align" style="margin: 0 auto;">
+						<a href="https://www.ippfwhr.org/" rel="external" target="_blank" class="ui-link">
+		<img class="newAboutIcon50" src="images/logo_ippf.png" title="IPPF"></a>
+					</div>
+				</div>
+			</p>
+
+			<!-- RETURN MENU -->
+			<a class="waves-effect waves-light btn back-button" href="#/anchor#menu" translate="back"></a>
+
+			<h3 id='fundacionHuesped'>Fundación Huésped</h3>
+			<p translate="about_fh_description"></p>
+			<p translate="about_fh_description_2"></p>
+
+			<p><b translate="about_title_follow"></b>
+
+				<div class="row">
+
+					<div class="col s12 m4 center-align newAboutLiMargin">
+						<a href="https://twitter.com/fundhuesped" rel="external" target="_blank" class="ui-link">
+	<i class="fa fa-twitter fa-4x"></i>Twitter</a>
+					</div>
+
+					<div class="col s12 m4 center-align newAboutLiMargin">
+						<a href="https://www.facebook.com/FundHuesped" rel="external" target="_blank" class="ui-link">
+	<i class="fa fa-facebook fa-4x"></i>Facebook</a>
+					</div>
+
+					<div class="col s12 m4 center-align newAboutLiMargin">
+						<a href="https://www.instagram.com/fundhuesped" rel="external" target="_blank" class="ui-link">
+		<i class="fa fa-instagram fa-4x"></i> Instagram</a>
+					</div>
+
+				</div>
+			</p>
+
+			<p><b translate="about_title_visit_page"></b>
+				<div class="row">
+					<div class="col s12 m12 center-align" style="margin: 0 auto;">
+						<a href="https://www.huesped.org.ar/" rel="external" target="_blank" class="ui-link">
+		<img class="newAboutIcon50" src="images/logo_huesped.png" title="IPPF"></a>
+					</div>
+				</div>
+			</p>
+
+			<!-- RETURN MENU -->
+			<a class="waves-effect waves-light btn back-button" href="#/anchor#menu" translate="back"></a>
+
+				<h3 id="comoFunciona" translate="about_href_how_it_works"> </h3>
+				<h4 translate="about_title_find_services"> </h4>
+				<p translate="about_description_find_services"></p>
+
+				<h4 translate="about_title_evaluate_services"></h4>
+				<p translate="about_description_evaluate_services"></p>
+
+				<h4 translate="about_title_add_info">  </h4>
+
+			<p>
+				<span translate="about_descripcion1_add_info"></span>
+				<a href="https://vamoslac.org/form" rel="external" target="_blank" class="ui-link" translate="about_description2_add_info"></a>
+				<span translate="about_description3_add_info"></span>
+				<img class="newAboutIcon+" src="images/about/+new_btn.png" width="32px" title="IPPF">
+				<span translate="about_description4_add_info"></span>
+				<span translate="about_description5_add_info"></span>
+				<a href="mailto:vamoslacapp@gmail.com" rel="external" target="_blank" class="ui-link" translate="about_description6_add_info"></a>
+			</p>
+
+			<!-- RETURN MENU -->
+			<a class="waves-effect waves-light btn back-button" href="#/anchor#menu" translate="back"></a>
+
+	</div> {{-- modal content --}}
+
 	<div class="modal-footer">
 		<a href=""
 		class="modal-action modal-close btn-flat " translate="close_label_button"></a>
