bf538d32aa3b1d9a9840bc3ec94adb1130510902
Your Name <you@example.com>
2019-02-16 18:26:20 -0300

    detalle tooltip

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -10,10 +10,14 @@
       </a>
       <!-- DESKTOP NAVBAR -->
       <ul class="right hide-on-med-and-down">
-        <li><a class="modal-trigger" href="#modal"><i class="mdi-action-info"></i></a></li>
-        <li><a class="" href="#/localizar/all/listado"><i class="mdi-maps-place left"></i></a></li>
-        <li><a class="" href="form"><i class="mdi-content-add-circle-outline"></i></a></li>
-        <li><a class="" href="listado-paises"><i class="mdi-action-language"></i></a></li>
+        <li><a class="modal-trigger tooltipped" 
+          data-position="bottom" data-tooltip="Mas información"  href="#modal"><i class="mdi-action-info"></i></a></li>
+        <li><a class="tooltipped" 
+          data-position="bottom" data-tooltip="Lugares cercanos" href="#/localizar/all/listado"><i class="mdi-maps-place left"></i></a></li>
+        <li><a class="tooltipped" 
+          data-position="bottom" data-tooltip="Sugerir nuevo lugar" href="form"><i class="mdi-content-add-circle-outline"></i></a></li>
+        <li><a class="tooltipped" 
+          data-position="bottom" data-tooltip="Buscar por listado" href="listado-paises"><i class="mdi-action-language"></i></a></li>
       </ul>
 
      <!-- POP NAVIGATION -->
