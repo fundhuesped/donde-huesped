95d1e95dcf4ed4cc78b10178e266abf944fe7076
Nicolas Eckell <nicolas@genosha.com.ar>
2020-01-28 16:08:33 -0300

    Mejoras de usabilidad en Gestión de Localidades Activas
    -Se mejoró la usabilidad pretendida de la página de gestión de localidades activas (del menú, localidades y provincias): conexión con API, paginación, orden por nombre, orden por cantidad. Se agrega también la posibilidad de ordenar por los otros campos (habilitado, depto, provincia, pais).
    -Se agregaron también algunos ajustes visuales:
    *Se deja solo un loader mientras se espera que el servidor responda (antes era dos innecesarios).
    *Si la búsqueda es corta (<10), entonces se muestra un solo panel de paginación (Anterior - Pag - Siguiente) en la parte de abajo, sino se muestra arriba y abajo.
    *La "limpieza automática" era muy molesta visualmente para utilizar el buscador, por lo que se agrega un botón para desplegar u ocultar dicho panel en caso de que no sea de interés visualizarlo.
    *Se agrega un botón "Borrar" (color gris, denota poca importancia), que lo que hace es reiniciar la búsqueda y volver al inicio de como se cargó la página inicialmente.
    *El color del botón "Buscar" se cambia a azul para no abusar con el color rojo en toda la página.
    
    -Se agrega una nueva directiva al "core" de la app en el panel, la cual es "ngEnter", y permite que cuando se presione la tecla enter en el input seleccionado, dispare una función asociada (normalemnte, es la que dispara el tag <a>). Esto sirve para no tener que presionar para realizar una acción, sino que también se pueda hacer por la tecla "enter".
    *Se agregan nuevos elementos css para una mejor visualización de la página presente.

--- resources/views/panel/city-list.blade.php
+++ resources/views/panel/city-list.blade.php
@@ -4,17 +4,14 @@
 {{ csrf_field() }}
 
 <div class="home panel" ng-controller="cityListController">
-  <div class="row">
-    <div class="col s4">
-      <p> </p>
-    </div>
-    <div class="col s4">
-      <h2> Limpieza Automatica </h2>
+  <div class="row" ng-hide="!cleaningLabel">
+    <div class="col offset-s3 s6">
+      <h2> Limpieza Automática </h2>
       <p> Eliminá todos las regiones que no tengan centros con un click </p>
       <div ng-cloak ng-show="spinner" class="progress">
         <div class="indeterminate"></div>
       </div>
-      <div ng-hide="spinner">
+      <div class="col offset-s2 s8" ng-hide="spinner">
         <div class="row">
           <div ng-cloak ng-show="loadingCiudades" class="progress">
             <div class="indeterminate"></div>
@@ -52,30 +49,41 @@
     </div>
   </div>
   <div class="row">
-    <br>
-    <br>
-    <hr />
-    <h3> Busca y Administra Ciudades</h3>
-
-
+    <hr/ ng-hide="!cleaningLabel" class="mt-3">
+    <div class="col offset-s4 s4">
+      <h3>Busca y Administra Ciudades</h3>
+    </div>
+    <div class="col offset-s1 s3">
+      <a href="" ng-click="cleaningLabel = !cleaningLabel" class="waves-effect waves-light btn wow animated orange" style="visibility: visible;" title="Despliega/oculta el panel de limpieza">
+        <span class="ng-scope">Limpieza automática</span>
+      </a>
+    </div>
   </div>
-  <div class="section navigate row">
-    <h2 ng-cloak ng-show="loadingPrev" traslate="panel_places_loadingplaces"> </h2>
+  <div class="section navigate row" ng-show="loadingPrev">
+    <h2 ng-cloak traslate="panel_places_loadingplaces"> </h2>
   </div>
-  <div class="section copy row">
+  <div class="mt-1 copy row">
     <div class="col s12 m12 ">
-      <p ng-cloak ng-hide="loadingPrev" translate="panel_places_summary" translate-values="{cities:'[[cities.total]]'}"> </p>
-
-      <div class="col s10">
-        <input type="text" ng-model="search" rol="search" placeholder="Ingresar una ciudad" />
+      <div class="row mt-2">
+      <div class="col s8">
+        <input type="text" ng-enter="loadPage()" ng-model="search" rol="search" placeholder="Ingresar una ciudad" />
       </div>
       <div class="col s2">
-        <a href="" ng-click="loadPage()" class="waves-effect waves-light btn wow animated" style="visibility: visible;">
+        <a href="" ng-click="loadPage()" class="waves-effect waves-light btn wow animated blue" style="visibility: visible;">
           <span class="ng-scope">Buscar</span>
         </a>
       </div>
-      <div class="col s12">
-        <div class="row" ng-hide="loadingPrev">
+      <div class="col s2">
+        <a href="" ng-click="clearResults()" class="waves-effect waves-light btn wow animated grey" style="visibility: visible;" title="Reinicia la búsqueda realizada">
+          <span class="ng-scope">Borrar</span>
+        </a>
+      </div>
+    </div>
+    <div class="row">
+      <p ng-cloak ng-hide="loadingPrev" translate="panel_places_summary" translate-values="{cities:'[[cities.total]]'}"> </p>
+    </div>
+      <div class="mt-1 col s12">
+        <div class="row" ng-hide="loadingPrev || cities.length < bottomPaginateLimit">
           <div class="col s2">
             <a href="" ng-click="previousPage()" class="waves-effect waves-light btn wow animated" style="visibility: visible;">
               <i class="mdi-navigation-chevron-left left"></i>
@@ -96,28 +104,22 @@
         </div>
       </div>
 
-      <table class="bordered striped responsive-table">
-
+      <table class="bordered striped responsive-table orderded">
         <thead>
           <tr ng-cloak>
-
-            <th data-field="nombre" translate="panel_places_columntable_5"></th>
-            <th data-field="nombre" translate="panel_places_columntable_1"></th>
-            <th data-field="nombre" translate="panel_places_columntable_2"></th>
-            <th data-field="nombre" translate="panel_places_columntable_6"></th>
-            <th data-field="nombre" translate="panel_places_columntable_3"></th>
-            <th data-field="nombre" translate="panel_places_columntable_4"></th>
-
+            <th ng-click="orderWith('nombre_ciudad')" data-field="nombre_ciudad" translate="panel_places_columntable_5"></th>
+            <th ng-click="orderWith('nombre_partido')" data-field="nombre_partido" translate="panel_places_columntable_1"></th>
+            <th ng-click="orderWith('nombre_provincia')" data-field="nombre_provincia" translate="panel_places_columntable_2"></th>
+            <th ng-click="orderWith('nombre_pais')" data-field="nombre_pais" translate="panel_places_columntable_6"></th>
+            <th ng-click="orderWith('habilitado')" data-field="habilitado" translate="panel_places_columntable_3"></th>
+            <th ng-click="orderWith('countPlaces')" data-field="countPlaces" translate="panel_places_columntable_4"></th>
           </tr>
         </thead>
         <div ng-cloak ng-show="loadingPrev" class="progress">
           <div class="indeterminate"></div>
         </div>
-        <div ng-cloak ng-show="spinner" class="progress">
-          <div class="indeterminate"></div>
-        </div>
         <tbody ng-hide="loadingPrev" ng-show="cities.length > 0">
-          <tr ng-cloak ng-hide="loadingPrev" ng-repeat="city in cities">
+          <tr ng-cloak ng-hide="loadingPrev" ng-repeat="city in cities | orderBy:dynamicOrderFunction">
             <td>[[city.nombre_ciudad]]</td>
             <td>[[city.nombre_partido]]</td>
             <td>[[city.nombre_provincia]]</td>
